bcfca4a80b3a12afc96ca486b9d5ce8836d286f5
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2026-02-23 23:16:15 -0300

    Conclusão de exercicío concatenação de dados do IPEA

--- exercicios/case_homicidios/case.py
+++ exercicios/case_homicidios/case.py
@@ -1,18 +1,30 @@
 # %%
 import pandas as pd
-
-df_geral = pd.read_csv("../../data/ipea/homicidios.csv", sep=";")
-df_geral = df_geral.rename(columns={"valor": "homicidio"})
-df_geral = df_geral.set_index(["nome", "período"])
-df_geral = df_geral.drop(["cod"], axis=1)
-df_geral.head()
+import os
 
 # %%
 
-df_negros = pd.read_csv("../../data/ipea/homicidios-negros.csv", sep=";")
-df_negros = df_negros.rename(columns={"valor": "homicidio"})
-df_negros = df_negros.set_index(["nome", "período"])
-df_negros = df_negros.drop(["cod"], axis=1)
-df_negros.head()
+def read_file(file_name:str):
+    df = (pd.read_csv(f"../../data/ipea/{file_name}.csv", sep=";")
+            .rename(columns={"valor":file_name})
+            .set_index(["nome", "período"])
+            .drop(["cod"], axis=1))
+    
+    return df
+
 # %%
-pd.concat([df_geral, df_negros], axis=1)
+
+file_names = os.listdir("../../data/ipea")
+
+dfs = []
+
+for i in file_names:
+    file_name = i.split(".")[0]
+    dfs.append(read_file(file_name))
+
+# %%
+df_full = pd.concat(dfs, axis=1).reset_index().sort_values(["período", "nome"])
+
+# %%
+
+df_full.to_csv("homicidios_consolidados.csv", index=False, sep=";")
