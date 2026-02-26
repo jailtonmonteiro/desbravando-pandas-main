6db987a88692f151f7e1848222e1e9212e5d1044
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2026-02-26 10:34:55 -0300

    Dia 09 - Conexão com o banco de dados

--- dia09/02_etl.py
+++ dia09/02_etl.py
@@ -1,9 +1,28 @@
 # %%
-
 import pandas as pd
 import sqlalchemy
 
-# %%
+from sklearn import cluster
 
+# %%
+with open("etl.sql") as open_file:
+    query = open_file.read()
+
+print(query)
+# %%
 engine = sqlalchemy.create_engine("sqlite:///../data/database.db")
+
+df = pd.read_sql_query(query, con=engine)
+df
+# %%
+kmean = cluster.KMeans(n_clusters=4)
+kmean.fit(df[["totalproduct", "qtdTransacoes"]])
+
+df["cluester"] = kmean.labels_
+df
+# %%
+df.to_sql("transacao_cluster",
+            con=engine,
+            index=False,
+            if_exists="replace")
 # %%
